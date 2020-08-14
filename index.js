a89447fdaf54ad3416158fe116aac3f05c38ddf9
GustavoKruger <gustavokruger90@gmail.com>
2020-08-13 21:00:21 -0300

    first server response

--- index.js
+++ index.js
@@ -4,6 +4,9 @@
 const express = require("express");
 const app = express();
 
+app.get("/", (req, res) => {
+  res.send("<h1>first server response <h1/>");
+});
 // definir a porta que o servidor escutará
 const PORT = 1234;
 
